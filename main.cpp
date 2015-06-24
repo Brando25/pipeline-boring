cb6281b8fb3a4db56c768ae175a607ae022d0f1b
Brando25 <brandonwadkins@yahoo.com>
2015-06-24 08:11:26 -0500

    implemented database query.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: BrandonW
+ *
+ * Created on June 24, 2015, 8:06 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
